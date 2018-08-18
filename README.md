81faadb26e619cd64fae0e0ccf5bb604770f8ef1
Keith <keithtmccartney@hotmail.com>
2018-08-18 19:56:03 +0100

    Applying a 'FormatToolbar' component, integrating it into the project via structure & visual changes; applying README changes.

--- README.md
+++ README.md
@@ -6,16 +6,21 @@ Grab the article at [https://codeburst.io/lets-build-a-customizable-rich-text-ed
 ## Tips
 
 * Check out [this](https://css-tricks.com/snippets/css/system-font-stack/) clever explanation by css-tricks for more in detail
+* If you're unsure about props.children — [this](https://learn.co/lessons/react-this-props-children) article is good at explaining the concept behind children props in React. ✌️
 
-## What are some of the key features a rich text editor has? Hmm — let’s see (some of this is still being worked-on):
+## What are some of the key features a rich text editor has? Hmm — let's see (some of this is still being worked-on):
 
 * Images
 * Headers
 * Code highlighting
 * Embedded Videos
-* Text formatting (alignment, bullet list, ordered list, etc)
+* Text formatting (alignment, bullet list, ordered list, etc) (integrating the wmira [react-icons-kit](https://github.com/wmira/react-icons-kit) package)
 * Save the text to a database for safe keeping
 * Links
+
+## Thoughts
+
+"There's no shortage of remarkable ideas, what's missing is the will to execute them. — Seth Godin"
 
 ## Thanks
 
--- slate-react-rich-text-editor/src/components/TextEditor.js
+++ slate-react-rich-text-editor/src/components/TextEditor.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Editor } from 'slate-react'
 import { Value } from 'slate'
-import BoldMark from './BoldMark';
-import ItalicMark from './ItalicMark';
+import Icon from 'react-icons-kit';
+import { bold } from 'react-icons-kit/feather/bold';
+import { italic } from 'react-icons-kit/feather/italic';
+import { BoldMark, ItalicMark, FormatToolbar } from './index';
 
 // Create our initial value...
 const initialValue = Value.fromJSON({
@@ -80,12 +82,24 @@ export default class TextEditor extends Component {
 
     render() {
         return(
-            <Editor
-                value={this.state.value}
-                onChange={this.onChange}
-                onKeyDown={this.onKeyDown}
-                renderMark={this.renderMark}
-            />
+            <Fragment>
+                <FormatToolbar>
+                    <button className="tooltip-icon-button">
+                        <Icon icon={bold} />
+                    </button>
+
+                    <button className="tooltip-icon-button">
+                        <Icon icon={italic} />
+                    </button>
+                </FormatToolbar>
+
+                <Editor
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyDown}
+                    renderMark={this.renderMark}
+                />
+            </Fragment>
         )
     }
 }
